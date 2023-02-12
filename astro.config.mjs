b65db8e3c6d80226ee67de5d73f8f4ffd52e06c2
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2023-02-13 02:21:49 +0530

    Revert "change to dege vercel"
    Using direct url for now
    
    This reverts commit 7d8f1634cecf88efefa484db3dc732f815ed88f8.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
